4cf76fd7e921e448eef5468482b9c8cdb9722b9f
bclarke329 <bmendoza329@hotmail.com>
2021-09-10 16:52:28 -0500

    added a little css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
+import Resume from './components/Resume';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
@@ -19,6 +20,7 @@ function App() {
           <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/about" component={About} />
+              <Route exact path="/resume" component={Resume} />
               <Route exact path="/portfolio" component={Portfolio} />
               <Route exact path="/contact" component={Contact} />
           </Switch>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,11 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/portfolio">Portfolio</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/">Home |</NavLink> 
+            <NavLink to="/about">About |</NavLink>
+            <NavLink to="/resume">Resume |</NavLink>
+            <NavLink to="/portfolio">Portfolio |</NavLink>
+            <NavLink to="/contact">Contact |</NavLink>
         </div>
     )
 }
