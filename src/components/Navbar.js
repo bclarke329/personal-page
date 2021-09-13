28a952a3a28f53a2b97c9b6f3cf0871f7aaf8e13
bclarke329 <bmendoza329@hotmail.com>
2021-09-13 18:19:29 -0500

    fixed padding between nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className="navbar">
-            <NavLink exact activeClassName="active" to="/">Home | </NavLink> 
-            <NavLink exact activeClassName="active" to="/about">About |</NavLink>
-            <NavLink exact activeClassName="active" to="/resume">Resume |</NavLink>
-            <NavLink exact activeClassName="active" to="/portfolio">Portfolio |</NavLink>
+            <NavLink exact activeClassName="active" className="navigation-links" to="/">Home  </NavLink> 
+            <NavLink exact activeClassName="active" className="navigation-links"to="/about">About </NavLink>
+            <NavLink exact activeClassName="active" className="navigation-links"to="/resume">Resume </NavLink>
+            <NavLink exact activeClassName="active" className="navigation-links"to="/portfolio">Portfolio </NavLink>
         </div>
     )
 }
