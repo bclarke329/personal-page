88bbd9881583087e028d723b08e04cafb9f57ef2
bclarke329 <bmendoza329@hotmail.com>
2021-09-14 19:29:27 -0500

    fixed destorted headshot

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Headshot from '../images/bessyheadshot.jpeg'
+import CircleHeadshot from '../images/circle-headshot.png'
 
 class Home extends Component {
     render() {
         return (
             <div className="name">
                 <h1>Bessy S. Clarke</h1>
-                <img src={Headshot} className="circle-photo"></img>
+                <img src={CircleHeadshot} alt="bessy's headshot" id="circle-headshot"></img>
                 <p className="bio">Full-Stack Web Developer with experience in JavaScript, React, Redux, Ruby On Rails, HTML, CSS</p>
             </div>
         );
