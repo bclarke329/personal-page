66532cd5a80ae5b0a132ada81b70d57486bfef97
bclarke329 <bmendoza329@hotmail.com>
2021-10-08 22:40:22 -0500

    building out professional component

---
+++ src/components/ProfessionalExp.js
@@ -0,0 +1,26 @@
+
+const ProfessionalExp = () => {
+    return (
+        <div>
+            <h3>Professional Experience</h3>
+            <p>Mop A Glow: Office Manager</p>
+            <p>New Orleans, LA</p>
+            <p>05/2015-Present</p>
+            <p>Manage all operations for a cleaning company that has over 200 clients and 25 employees</p>
+            <ul>
+                <li>Responsible for all business functions, including weekly bank deposits, managing finances, sending and tracking invoices, paying all company bills, and equipment procurement</li>
+                <li>Initiated operational improvements, such as: starting worker's comp program, securing business grants and loans, and purchasing general liablity insurance</li>
+                <li>Serving as the main English to Spanish translator between clients and predominately Spanish-speaking staff </li>
+            </ul>
+            <p>Brown Butter: Server</p>
+            <p>New Orleans, LA</p>
+            <p>10/2018-Present</p>
+            <ul>
+                <li>Maintained a high level of customer satisfaction, including retaining repeat clients and receiving multiple 5 star reviews</li>
+                <li>Ability to work multiple positions during high volume hours </li>
+            </ul>
+        </div>
+    )
+
+    export default ProfessionalExp
+}
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import TechProjects from './TechProjects';
 
 class Resume extends Component {
     render() {
         return (
             <div>
                 <h1>Resume</h1>
-                
+               
+                <TechProjects />
                 
                 
             
