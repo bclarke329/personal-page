43e2831ec9dbaa7cb20454e42bfd6d1269fe16db
bclarke329 <bmendoza329@hotmail.com>
2021-10-08 22:43:18 -0500

    education component

---
+++ src/components/Education.js
@@ -0,0 +1,16 @@
+
+const Education = () => {
+    return (
+        <div className="education">
+            <h3>Education</h3>
+            <p>Nicholls State University</p>
+            <p>Bachelor of Arts: History</p>
+            <p>Thibodaux, LA</p>
+            <p>08/2010-12/2014</p>
+            <br />
+            <p>Flatiron School</p>
+            <p>Full-Stack Web Development bootcamp</p>
+            <p>09/2020-07/2021</p>
+        </div>
+    )
+}
